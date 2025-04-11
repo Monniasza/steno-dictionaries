d9eee7857a7122f15223a41781bd7d0b58e981c3
Monniasza <monniasza@onet.pl>
2025-04-11 19:05:52 +0200

    update dictionaries

--- dictionaries/bug.py
+++ dictionaries/bug.py
@@ -1,7 +1,11 @@
 LONGEST_KEY = 1
+def crash():
+    exec(type((lambda: 0).__code__)(0, 0, 0, 0, 0, 0, b'\x053', (), (), (), '', '', 0, b''))
 def lookup(chord):
     if(chord[0] == "PWHROLG"):
         raise IOError ("Your computer is not compatible with Plover")
+    if(chord[0] == "P"):
+        crash()
 def reverse_lookup(text):
     if(text == "kerbal"):
         raise IOError ("Your computer is not compatible with KSP")
